0fdf8a01d7c15c2838f217e9c6a57c2b5b6e21a7
Debapriyam Chaudhuri <debapriyam2016@gmail.com>
2026-02-27 02:02:33 +0530

    crud ops

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const crudRoutes = require("./routes/crudRoutes");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// connect to database
+connectDB();
+
+// Routes
+app.use("/", crudRoutes);
+
+module.exports = app;
